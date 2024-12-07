1781ed7a62ff53d60bd26214965a00c274ad55ad
Ujjwal Mishra <ptujjwal663@gmail.com>
2024-12-07 06:58:07 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+// Optional: Add interactivity here, such as animations or smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
